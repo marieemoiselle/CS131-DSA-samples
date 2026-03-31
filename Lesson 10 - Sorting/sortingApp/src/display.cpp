aceabfb91a2ad9249fabea9d62c870107f0c74de
marieemoiselle <agdonmarie@gmail.com>
2026-03-31 11:22:00 +0800

    set width for elements

--- Lesson 10 - Sorting/sortingApp/src/display.cpp
+++ Lesson 10 - Sorting/sortingApp/src/display.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include "sorting.h"
 using namespace std;
 
 void display(int arr[], int n) {
-    for(int i = 0; i < n; i++) cout << arr[i] << " ";
+    for(int i = 0; i < n; i++) cout << setw(4) << arr[i] << " ";
     cout << endl;
 }
